8c22e0666444e2ac9cd2d7df16137bd68121e806
Alex <guardamar81@gmail.com>
2024-05-17 20:53:11 +0400

    appa.png

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Игра")
 
+image = pygame.image.load("img/appa.png")
+image = image.get_rect()
 
 
 
@@ -14,3 +16,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
